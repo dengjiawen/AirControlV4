628dcb7b4ebf5702c41a292d5f8966b842eb3457
Phreaddie <freddengmath@gmail.com>
2018-06-12 23:45:43 -0400

    update 9
    Minor bug fixes.

--- src/main/java/path/Node.java
+++ src/main/java/path/Node.java
@@ -1,20 +1,49 @@
+/**
+ * Copyright 2018 (C) Jiawen Deng. All rights reserved.
+ * <p>
+ * This document is the property of Jiawen Deng.
+ * It is considered confidential and proprietary.
+ * <p>
+ * This document may not be reproduced or transmitted in any form,
+ * in whole or in part, without the express written permission of
+ * Jiawen Deng.
+ * <p>
+ * -----------------------------------------------------------------------------
+ * Node.java
+ * -----------------------------------------------------------------------------
+ * This is a position object, used for guiding the plane.
+ * <p>
+ * This class is a part of the AssistLogic.
+ * -----------------------------------------------------------------------------
+ */
+
 package main.java.path;
 
 import java.awt.geom.Point2D;
+
 import java.io.Serializable;
 
 public class Node extends Point2D implements Serializable {
 
-    private double x;
-    private double y;
+    private double x;   // x pos of the node
+    private double y;   // y pos of the node
 
-    private Node next_node;
-    private Node prev_node;
+    private Node next_node; // the previous node
+    private Node prev_node; // the next node
 
-    protected NodeType type;
+    NodeType type;  // the type of node
 
+    /**
+     * Default constructor
+     */
+    private Node() {}
+
+    /**
+     * Constructor
+     */
     protected Node(Point2D p, Node prev_node, Node next_node) {
 
+        /* instantiate instance variables */
         setLocation(p);
         this.next_node = next_node;
         this.prev_node = prev_node;
