a8da5ebda0d3c905616c45b31268cec9cbcd1b2b
Phreaddie <freddengmath@gmail.com>
2018-06-13 15:17:44 -0400

    update 21
    Fixed a bug in the MusicUtils class.

--- src/main/java/music/MusicUtils.java
+++ src/main/java/music/MusicUtils.java
@@ -20,29 +20,34 @@ public class MusicUtils {
 
         interval = Constants.getInt("interval", Definitions.BOMBARDIER_PATCH);
         music_count = Constants.getInt("musicCount", Definitions.BOMBARDIER_PATCH);
-
-        int initial_music_pick = ThreadLocalRandom.current().nextInt(0, music_count);
-        active_music = TinySound.loadMusic("/music/" + initial_music_pick + ".ogg", true);
-        active_music.play(false);
+        last_played = -1;
 
         TinySound.init();
+
+        pickNewMusic();
 
         Timer music_regulator = new Timer(interval, e -> {
 
             if (!active_music.done()) return;
-
-            int music_pick = ThreadLocalRandom.current().nextInt(0, music_count);
-            while (music_pick == last_played) {
-                music_pick = ThreadLocalRandom.current().nextInt(0, music_count);
-            }
-
-            active_music = TinySound.loadMusic("/music/" + music_pick + ".ogg", true);
-            active_music.setVolume(0.75f);
-            active_music.play(false);
+            pickNewMusic();
 
         });
 
         music_regulator.start();
+
+    }
+
+    public static void pickNewMusic() {
+
+        int music_pick = ThreadLocalRandom.current().nextInt(0, music_count);
+        while (music_pick == last_played) {
+            music_pick = ThreadLocalRandom.current().nextInt(0, music_count);
+        }
+
+        active_music = TinySound.loadMusic("/music/" + music_pick + ".ogg", true);
+        active_music.play(false);
+
+
 
     }
 
