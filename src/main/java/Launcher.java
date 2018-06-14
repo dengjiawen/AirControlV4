0d9fd4951a184d0407c39fd228227ee856d3b175
Phreaddie <freddengmath@gmail.com>
2018-06-14 13:52:16 -0400

    update 27
    Minor bug fixes.

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2018 (C) Jiawen Deng. All rights reserved.
+ * <p>
+ * This document is the property of Jiawen Deng.
+ * It is considered confidential and proprietary.
+ * <p>
+ * This document may not be reproduced or transmitted in any form,
+ * in whole or in part, without the express written permission of
+ * Jiawen Deng.
+ * <p>
+ * -----------------------------------------------------------------------------
+ * Launcher.java
+ * -----------------------------------------------------------------------------
+ * This class is the entry point for the program.
+ * -----------------------------------------------------------------------------
+ */
+
 package main.java;
 
 import main.java.common.BlurUtils;
@@ -15,9 +32,14 @@ import main.java.ui.Window;
 
 public class Launcher {
 
+    /**
+     * Static block that initiates all of the different classes.
+     */
     static {
 
         LogUtils.init();
+
+        LogUtils.printCoreMessage("Initializing launcher...");
 
         BlurUtils.init();
 
@@ -25,7 +47,11 @@ public class Launcher {
         ImageResource.init();
         FontResource.init();
 
+        /* try to read data written by Paths */
+        /* if reading is not successful, tell MapUtil to recalculate paths */
+        LogUtils.printCoreMessage("Launcher is trying to read saved telemetry data...");
         if (!Paths.readTelemetryData()) {
+            LogUtils.printCoreMessage("Launcher cannot read saved telemetry data! Will recalculate.");
             MapUtils.init();
             Paths.saveTelemetryData();
         }
@@ -35,11 +61,19 @@ public class Launcher {
 
         CloudDirector.init();
 
+        LogUtils.printCoreMessage("Launcher is requesting garbage collection...");
         System.gc();
     }
 
+    /**
+     * Main method
+     * Creates a new Window object, and initializes
+     * the RenderUtils class.
+     * @param args
+     */
     public static void main (String... args) {
 
+        LogUtils.printCoreMessage("Launcher is initializing the main window.");
         Window window = new Window();
 
         RenderUtils.init();
--- src/main/java/ui/FrostedPane.java
+++ src/main/java/ui/FrostedPane.java
@@ -1,3 +1,5 @@
+
+
 package main.java.ui;
 
 import main.java.common.BlurUtils;
